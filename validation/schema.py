822ce9299247960094700485c31fabdb5c204148
KrisztianToth <krisztian.toth@labela.nl>
2018-07-23 13:34:41 +0200

    organize code into methods

--- validation/schema.py
+++ validation/schema.py
@@ -1,10 +1,15 @@
-from validation.validators import construct_error
 import typing
+
+from validation.validators import construct_error
 
 
 class Schema:
 
     def __new__(cls, **kwargs):
+        """
+        Collects mandatory fields into a list and binds it to the class.
+        Only has to run once for every class, hence the `_processed` property.
+        """
         if not hasattr(cls, '_processed'):
             mandatory_fields = []
             for k in cls.__annotations__.keys():
@@ -23,48 +28,15 @@ class Schema:
 
     def __init__(self, **kwargs):
         mandatory_fields = self._mandatory_fields.copy()
-        annotations = self.__class__.__annotations__
 
         class_items = self.__class__.__dict__
         self._validation_errors = []
         # set attributes
         for k, v in kwargs.items():
-            if not hasattr(self, k):
-                self._validation_errors.append(
-                    construct_error(k, "unknown attribute"))
+            self._check_unknown_attributes(k)
 
             # check if type matches the annotated type
-            type_ = type(v)
-            annotated_type = annotations[k]
-            if v is None:
-                # if the value is None, it's type will be NoneType
-                # we check for mandatory value assignment later
-                pass
-            elif type(annotated_type) is typing.GenericMeta and \
-                    issubclass(annotated_type, list):
-                # check typing.List
-                try:
-                    list_element_type = annotated_type.__args__[0]
-                    for elem in v:
-                        if not type(elem) is list_element_type:
-                            self._validation_errors.append(
-                                construct_error(
-                                    k, "wrong type in list. expected: "
-                                       "'%s', provided: '%s'"
-                                       % (list_element_type, type(elem)))
-                            )
-                            break
-                except (IndexError, TypeError):
-                    # if we get either of these exceptions it means that the
-                    # annotation is a generic List with no type hints for it's
-                    # content
-                    pass
-
-            elif not type_ == annotated_type:
-                self._validation_errors.append(
-                    construct_error(k, "wrong type. expected: '%s',"
-                                       "provided: '%s'"
-                                    % (annotated_type, type_)))
+            self._validate_type(k, v)
 
             # remove provided attributes from the mandatory list
             # this is necessary to accept provided `None` values as well
@@ -79,6 +51,59 @@ class Schema:
             setattr(self, k, v)
 
         # check if all mandatory fields have been set
+        self._check_mandatory_fields(mandatory_fields)
+
+    def _check_unknown_attributes(self, attr_name):
+        """
+        Checks for unknown attributes.
+
+        :param attr_name: the attribute's name
+        """
+        if not hasattr(self, attr_name):
+            self._validation_errors.append(
+                construct_error(attr_name, "unknown attribute"))
+
+    def _validate_type(self, attr_name, attr_val):
+        """
+        Validates the type of the property based on the annotation.
+
+        :param attr_name: the name of the attribute
+        :param attr_val: the value of the attribute
+        :return:
+        """
+        type_ = type(attr_val)
+        annotated_type = self.__class__.__annotations__[attr_name]
+        if attr_val is None:
+            # if the value is None, it's type will be NoneType
+            # we check for mandatory value assignment later
+            pass
+        elif type(annotated_type) is typing.GenericMeta and \
+                issubclass(annotated_type, list):
+            # check typing.List
+            try:
+                list_element_type = annotated_type.__args__[0]
+                for elem in attr_val:
+                    if not type(elem) is list_element_type:
+                        self._validation_errors.append(
+                            construct_error(
+                                attr_name, "wrong type in list. expected: "
+                                           "'%s', provided: '%s'"
+                                           % (list_element_type, type(elem)))
+                        )
+                        break
+            except (IndexError, TypeError):
+                # if we get either of these exceptions it means that the
+                # annotation is a generic List with no type hints for it's
+                # content
+                pass
+
+        elif not type_ == annotated_type:
+            self._validation_errors.append(
+                construct_error(attr_name, "wrong type. expected: '%s',"
+                                           "provided: '%s'"
+                                % (annotated_type, type_)))
+
+    def _check_mandatory_fields(self, mandatory_fields):
         for mandatory in mandatory_fields:
             if getattr(self, mandatory) is None:
                 self._validation_errors.append(
