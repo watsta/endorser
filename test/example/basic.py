4f258a0daabbdbf7ea8d4cb08d653c896a648433
KrisztianToth <krisztian.toth@labela.nl>
2018-07-26 14:48:39 +0200

    create example

---
+++ test/example/basic.py
@@ -0,0 +1,39 @@
+from validation.converter import DocumentConverter
+from validation.schema import Schema
+from validation.validator import min_size
+
+
+class Address(Schema):
+    zip_code: str
+    house_number: int
+    addition: str = None
+
+
+class User(Schema):
+    email: str
+    username: str
+    firstname: str = None  # assigning None as default makes it optional
+    address: Address = None  # nest Schema classes
+
+    @min_size(5)
+    def validate_username(self, username):
+        """Validates the username field, it has to be at least 5 chars long"""
+        return username
+
+
+data = {
+    "email": "example@email.com",
+    "username": "krisz",
+    "address": {
+        "zip_code": "6757",
+        "house_number": 12,
+        "addition": "A1"
+    }
+}
+
+if __name__ == '__main__':
+    converter = DocumentConverter()
+    user = converter.convert(data, User)
+    assert user.email is "example@email.com"
+    assert type(user.address) is Address
+    assert user.address.zip_code is "6757"
